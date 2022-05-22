b4bce231e92eb472d9caccbf3673b1155ef52dd6
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2022-05-21 23:20:27 -0400

    first commit

---
+++ spring-boot-reactor/src/test/java/com/talentyco/reactor/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentyco.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
